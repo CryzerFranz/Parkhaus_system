7e5c458886ce4264a7e11389e43369a3403f2eda
z0r0k <158172827+z0r0k@users.noreply.github.com>
2025-04-09 10:36:58 +0200

    LCD funktion hinzugefügt
    LCD soll die aktuelle Uhrzeit anzeigen

---
+++ include/LCD.h
@@ -0,0 +1,27 @@
+#ifndef LCDTIMEDISPLAY_H
+#define LCDTIMEDISPLAY_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+class LCDTimeDisplay {
+private:
+    LiquidCrystal_I2C* lcd;
+    WiFiUDP ntpUDP;
+    NTPClient* timeClient;
+    uint8_t lcdColumns;
+    uint8_t lcdRows;
+    unsigned long lastUpdate;
+
+public:
+    LCDTimeDisplay(uint8_t lcdAddr = 0x27, uint8_t cols = 16, uint8_t rows = 2);
+    void begin();
+    void updateTime();
+    ~LCDTimeDisplay();
+};
+
+#endif
---
+++ src/LCD.cpp
@@ -0,0 +1,52 @@
+#include "LCDTimeDisplay.h"
+
+LCDTimeDisplay::LCDTimeDisplay(uint8_t lcdAddr, uint8_t cols, uint8_t rows) 
+    : lcdColumns(cols), lcdRows(rows), lastUpdate(0) {
+    lcd = new LiquidCrystal_I2C(lcdAddr, lcdColumns, lcdRows);
+    timeClient = new NTPClient(ntpUDP, "pool.ntp.org", 7200); // UTC+2 für Deutschland
+}
+
+void LCDTimeDisplay::begin() {
+    lcd->init();
+    lcd->backlight();
+    timeClient->begin();
+    
+    // WLAN-Verbindung herstellen
+    WiFi.begin("DEIN_WLAN_NAME", "DEIN_WLAN_PASSWORT");
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        lcd->print("Verbinden...");
+    }
+    
+    lcd->clear();
+    timeClient->update();
+}
+
+void LCDTimeDisplay::updateTime() {
+    unsigned long currentMillis = millis();
+    
+    // Update nur alle 1000ms (1 Sekunde)
+    if (currentMillis - lastUpdate >= 1000) {
+        timeClient->update();
+        
+        lcd->setCursor(0, 0);
+        lcd->print("Uhrzeit:");
+        
+        lcd->setCursor(0, 1);
+        String timeStr = timeClient->getFormattedTime();
+        lcd->print(timeStr);
+        
+        // Füllt restliche Zeile mit Leerzeichen
+        for (int i = timeStr.length(); i < lcdColumns; i++) {
+            lcd->print(" ");
+        }
+        
+        lastUpdate = currentMillis;
+    }
+}
+
+LCDTimeDisplay::~LCDTimeDisplay() {
+    delete lcd;
+    delete timeClient;
+}
